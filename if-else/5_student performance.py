e5eb061f80f51af3f96f3e63c01db76fc22c266c
tejashree-001 <tejusamai3111@gmail.com>
2026-08-09 22:03:37 +0530

    Add files via upload
    Write a PYTHON program to evaluate the student performance
          If % is >=90 then Excellent performance
          If % is >=80 then  Very Good performance
          If % is >=70 then Good performance
          If % is >=60 then average performance
          else Poor performance.

---
+++ if-else/5_student performance.py
@@ -0,0 +1,20 @@
+# Write a PYTHON program to evaluate the student performance
+# If % is >=90 then Excellent performance
+# If % is >=80 then  Very Good performance
+# If % is >=70 then Good performance
+# If % is >=60 then average performance
+# else Poor performance.
+
+per=int(input("Enter a percrntage of a student:"))
+if(per>100):
+    print("invalid percentage")
+elif(per>=90):
+    print("Excellent performance")
+elif(per>=80):
+    print("Very Good performance")
+elif("per>=70"):
+    print("Good performance")
+elif(per>=60):
+    print("average performance")
+else:
+    print("Poor performance")
