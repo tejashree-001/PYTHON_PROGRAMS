6f28b518527d5c5c0b2e728625c20f7481886aae
tejashree-001 <tejusamai3111@gmail.com>
2026-08-09 20:37:30 +0530

    Add files via upload
    Write a PYTHON program to find a largest of two numbers.

---
+++ if-else/2_largest of two numbers.py
@@ -0,0 +1,7 @@
+# Write a PYTHON program to find a largest of two numbers.
+a=int(input("Enter a number(a): "))
+b=int(input("Enter a number(b): "))
+if(a>b):
+      print(f"number a {a} is greater than number b {b} ")
+else:
+    print(f"number b {b} is greater than number a {a} ")
