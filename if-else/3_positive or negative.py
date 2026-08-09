43f9975c0d412b5b0369b39ac8f99de3545f33b3
tejashree-001 <tejusamai3111@gmail.com>
2026-08-09 20:54:26 +0530

    Add files via upload
    Write a PYTHON program that reads the number and check the no is positive or negative.

---
+++ if-else/3_positive or negative.py
@@ -0,0 +1,9 @@
+#Write a PYTHON program that reads the number and check the no is positive or negative.
+
+num=int(input("Enter a number:"))
+if(num>0):
+    print("The number is positive.")
+elif(num<0):
+    print("The number is negative.")
+else:
+    print("The number is Zero.")
