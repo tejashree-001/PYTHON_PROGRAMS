e7b2c5477a1fe8cd77cef5d08bd6b1579867f9df
tejashree-001 <tejusamai3111@gmail.com>
2026-08-09 12:57:57 +0530

    Add files via upload
    Write a PYTHON program that reads a value of n and check the number is zero or non zero value.

---
+++ if-else/1_zero or non zero value.py
@@ -0,0 +1,8 @@
+# Write a PYTHON program that reads a value of n and check the number is zero or non zero value.
+
+n=int(input("Enter a number(n): "))
+if n==0:
+    print("number enterd is zero.")
+else:
+    print("number enterd is non-zero")
+    
