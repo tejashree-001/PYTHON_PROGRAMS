335fe52cb7a480bf6a7dc5654b449bf4c60dd972
tejashree-001 <tejusamai3111@gmail.com>
2026-08-10 15:24:13 +0530

    Add files via upload
    Write a PYTHON program to check a year for leap year.

---
+++ if-else/9_leap year.py
@@ -0,0 +1,6 @@
+# Write a PYTHON program to check a year for leap year.
+year=int(input("Enter a year:"))
+if(year%4==0):
+    print(year,"is leap year.")
+else:
+    print(year,"is not a leap year.")
