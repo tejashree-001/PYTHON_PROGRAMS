8fe36228379d1b91029d7519bdc4514ccfffaa42
tejashree-001 <tejusamai3111@gmail.com>
2026-08-10 15:20:56 +0530

    Add files via upload
    Write a PYTHON program to check weather number is even or odd.

---
+++ if-else/8_even or odd.py
@@ -0,0 +1,7 @@
+# Write a PYTHON program to check weather number is even or odd.
+
+num=int(input("Enter a number:"))
+if(num%2==0):
+    print("Tne number is even.")
+else:
+    print("The number is odd.")
